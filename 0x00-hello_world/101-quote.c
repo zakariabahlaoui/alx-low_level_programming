ba2a6bc1be8b652abff5161a4160d1f75bfa742f
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-08-11 18:49:33 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,12 @@
  * main - a program print string whit quote
  *
  * Return: reurm 1
- * 
+ *
  */
 
 int main(void)
 {
-
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
-}	
+}
 
