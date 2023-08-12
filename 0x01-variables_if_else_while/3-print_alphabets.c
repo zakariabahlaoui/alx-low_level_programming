16779464f68392046e6995296b7dd59a4004e03a
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-08-12 02:56:50 +0100

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - assign a random number to variable
+ *
+ * Description: print positive if n is positive if
+ *
+ *  Return: return 0
+ */
+
+int main(void)
+{
+	char c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
