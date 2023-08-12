6080183155e759491fad1352f14b150652cc2070
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-08-12 03:15:19 +0100

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Description: print number 0 to 9
+ *
+ *  Return: return 0
+ */
+
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
+
