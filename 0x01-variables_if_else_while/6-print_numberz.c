eb79851637f305816ecd761b3df93dbfe7a17ade
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-08-12 03:21:58 +0100

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Description: print nuber using putchar
+ *
+ *  Return: return 0
+ */
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(c);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
