350222d7f921b11feb9430c8eb60a2e51603ba2e
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-08-12 02:31:03 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+/**
+ * main - assign a random number to variable
+ *
+ * Description: print positive if n is positive if
+ * negative ptint negative and if 0 ptint zwro
+ *
+ *  Return: return 0
+ *
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar('c');
+		c++;
+	}
+	return (0);
+}
