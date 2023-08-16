41c21767633034ce6d50a07638bd0590e15aef76
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-08-16 02:16:49 +0100

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -24,7 +24,24 @@ int _isalpha(int c)
 	{
 		if (c >= 'A' && c <= 'Z')
 			return (1);
+		ch2++;
 	}
 
+	return (0);
+}
+
+int main(void)
+{
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }
