dfb6269d7bb28e84cc606fe4b3eb6e1e5ee1666b
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-08-16 03:34:52 +0100

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -22,8 +22,7 @@ void jack_bauer(void)
 			_putchar((hr / 10) + '0');
 			_putchar((hr % 10) + '0');
 			_putchar(':');
-			d
-				_putchar((min / 10) + '0');
+			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');
 			_putchar('\n');
 			min++;
