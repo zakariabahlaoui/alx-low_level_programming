24f99f3b3c6d5964a12d2e23320a84c097e304b9
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-08-17 01:54:51 +0100

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,21 +8,21 @@
  */
 int main(void)
 {
-    int i = 0;
-    long j = 1, k = 2, sum = k;
+	int i = 0;
+	long j = 1, k = 2, sum = k;
 
-    while (k + j < 4000000)
-    {
-        k += j;
+	while (k + j < 4000000)
+	{
+		k += j;
 
-        if (k % 2 == 0)
-            sum += k;
+		if (k % 2 == 0)
+			sum += k;
 
-        j = k - j;
+		j = k - j;
 
-        ++i;
-    }
+		++i;
+	}
 
-    printf("%ld\n", sum);
-    return (0);
+	printf("%ld\n", sum);
+	return (0);
 }
