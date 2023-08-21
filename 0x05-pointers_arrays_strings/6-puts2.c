fe6e54bed0ae307290d692f7f5bd16bd04e30c96
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-08-22 00:39:29 +0100

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,13 +11,19 @@
 
 void puts2(char *str)
 {
-	int i;
+	int len, i;
 
-	i = -1;
-	while (str[i] != 0)
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
-		i += 2;
 	}
+
 	_putchar('\n');
 }
