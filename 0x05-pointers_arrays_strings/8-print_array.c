bd66026fad1260c832ecf5bdbcf60bd19a94a905
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-08-22 01:34:01 +0100

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
-		i++;
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
 	}
 	printf("\n");
 }
