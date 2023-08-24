8276bad077e3a2254aeccb83b5f546563800f804
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-08-25 00:17:17 +0100

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: the first string
+ * @src: ehe second to concatenate
+ * @n: the number the charachter to concatenate
+ *
+ * Return: return dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] && j <= n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
