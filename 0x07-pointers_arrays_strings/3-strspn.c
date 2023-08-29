60c27ebe7746596290dc22564bef8a0566586b0d
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-08-29 02:45:02 +0100

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -33,14 +33,3 @@ unsigned int _strspn(char *s, char *accept)
 
 	return (0);
 }
-
-int main(void)
-{
-	char *s = "hello, world";
-	char *f = "oleh";
-	unsigned int n;
-
-	n = _strspn(s, f);
-	printf("%u\n", n);
-	return (0);
-}
