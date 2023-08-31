d9956f55152381a2e0c9cf85b6036c0124ab6b3a
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-08-31 01:05:06 +0100

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints the characters in s recursively.
+ *
+ * @s: string to print
+ *
+ * Return: void.
+ */
+
+void _puts_recursion(char *s)
+{
+
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+		_putchar(*s);
+
+	_puts_recursion(s + 1);
+}
